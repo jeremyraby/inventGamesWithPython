80b66cbe826b7e76f6dc240d4d4c95d44e411cc2
jeremyraby <jeremy.raby10@gmail.com>
2020-02-22 19:22:52 -0600

    Initial commit

---
+++ jokes.py
@@ -0,0 +1,17 @@
+# Program tells jokes in a Q & A format
+import time
+
+print('What do you get when you cross a snowman with a vampire?')
+input()
+print('Frostbite!\n\n')
+
+print("What do dentists call an astronaut's cavity?")
+input()
+print('A black hole!\n\n')
+
+print('Knock knock.')
+input()
+print('Interrupting cow.')
+input()
+print('Interrupting cow wh', end='')
+print('-MOO!')
