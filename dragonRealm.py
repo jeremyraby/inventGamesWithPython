7d72dd7c89a1d5545aa8a0e4787efd216afad210
jeremyraby <jeremy.raby10@gmail.com>
2020-02-22 21:34:44 -0600

    Edited dragonRealm to have functions

--- dragonRealm.py
+++ dragonRealm.py
@@ -1,29 +1,44 @@
-import time
+import time, random
 
-# Introduction to the game
-print('''You are in a land full of dragons. In front of you,
-you see two caves. In one cave, the dragon is friendly
-and will share his treasure with you. The other dragon
-is greedy and hungry, and will eat you on sight.''')
-player_choice = int(input('Which cave will you enter? (1 or 2) \n'))
-# Create a loop for the game to run
-play_again = True
-while play_again == True:
+
+def display_intro():
+    # Introduction to the game
+    print('''You are in a land full of dragons. In front of you,
+    you see two caves. In one cave, the dragon is friendly
+    and will share his treasure with you. The other dragon
+    is greedy and hungry, and will eat you on sight.''')
+
+def choose_cave():
+    # Player chooses which cave to enter
+    cave = ''
+    while cave != 1 and cave != 2:
+        cave = int(input('Which cave will you enter? (1 or 2)'))
+    return cave
+
+def check_cave(chosen_cave):
     print('You approach the cave...')
     time.sleep(2)
     print("It's dark and spooky...")
     time.sleep(2)
-    print("A large dragon jumps out in fron of you! He opens his jaws and ...")
+    print("A large dragon jumps out in front of you! He opens his jaws and ...")
     time.sleep(2)
-# If/elif player goes in "hungry" or "friendly" cave
-    if player_choice == 1:
-        print('Gobbles you down in one bite!')
-    elif player_choice == 2:
+
+    # If/else player goes in "hungry" or "friendly" cave
+    friendly_cave = random.randint(1, 2)
+
+    if chosen_cave == friendly_cave:
         print('... says "How\'s it going? Want some pizza?"')
-# Ask to play again
-    play_again = input('Want to play again? (y or n) \n')
-    if play_again.lower() == 'y':
-        play_again = True
-    elif play_again.lower() == 'n':
-        print('Thanks for playing!')
-        break
+    else:
+        print('Gobbles you down in one bite!')
+
+# Create a loop for the game to run
+play_again = 'yes'
+while play_again == 'yes' or play_again == 'y':
+    display_intro()
+    cave_number = choose_cave()
+    check_cave(cave_number)
+    print('Do you want to play again? (yes or no)')
+    play_again = input()
+
+
+
